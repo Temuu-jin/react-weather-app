3c7c8eebc08ad3a19ea27ff2fe88779ff2f2daf6
Temuu-jin <temucin.d.k@gmail.com>
2023-09-11 16:33:57 +0200

    Add App Functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import './App.css';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 
-export default function App() {
+const Weather = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const API_KEY = process.env.API_Key;
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=Vienna&appid=${API_KEY}&units=metric`,
+        );
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.log('Error fetching weather data:', error);
+      }
+    };
+
+    fetchWeatherData().catch((error) => {
+      console.error('Error in fetchWeatherData:', error);
+    });
+  }, [API_KEY]);
+
+  if (weatherData === null || !weatherData.main) {
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor: 'skyblue',
+        textAlign: 'center',
+        marginLeft: 500,
+        marginRight: 500,
+        padding: 100,
+      }}
+    >
+      <div>
+        <h2>{weatherData.name}</h2>
+        <p>Temperature: {Math.round(weatherData.main.temp)}°C</p>
+        <p>Description: {weatherData.weather[0]?.description}</p>
+      </div>
     </div>
   );
-}
+};
+
+export default Weather;
